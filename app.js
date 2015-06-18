8c4774836e16f308983138fdf28a2057bb79f3a6
John Alfano <alf027@gmail.com>
2015-06-18 15:46:32 -0600

    refactored

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-//var turn = 'x';
+
 //var xChoices = [];
 //var oChoices = [];
 //var moveCount = 0;
 
 function moveCheck(num,arr,turn) {
-  if (num === 9 && winCheck(arr)===true) {
+  if (winCheck(arr)===true) {
     alert(turn + ' is the winner!');
     document.location.reload()
   } else if (num === 9){
@@ -12,20 +12,27 @@ function moveCheck(num,arr,turn) {
     document.location.reload()
   }
 }
-var playerMoves = {
-  'x':[],
-  'y':[]
-}
+
 
 function play(target,turn){
-  //var turn = 'x';
+  if(!turn) {
+    turn = 'x'
+  }
   var xChoices = [];
   var oChoices = [];
-  var moveCount = 0;
+
   var turnObj = {
     'x':"fa fa-times fa-5x",
-    'y':"fa fa-circle-o fa-5x"
-  }
+    'o':"fa fa-circle-o fa-5x"
+  };
+
+  target.dataset.avail = 'no';
+  target.childNodes[0].className = turnObj[turn]
+  target.childNodes[0].style.display = 'inline-block';
+  playerMoves[turn].push(target.dataset.loc);
+  moveCount ++;
+
+  moveCheck(moveCount,playerMoves[turn],turn);
 
 
 
@@ -33,46 +40,55 @@ function play(target,turn){
 
 
 }
-
+var moveCount = 0;
+var playerMoves = {
+  'x':[],
+  'o':[]
+};
+var turn = 'x';
 document.querySelector('body').addEventListener('click', function (event) {
 
   var target = event.target;
 
+
   if (target.dataset.avail === 'yes') {
-    if (turn === 'x') {
-      target.dataset.avail = 'no';
-      target.childNodes[0].className = "fa fa-times fa-5x";
-      target.childNodes[0].style.display = 'inline-block';
-      xChoices.push(target.dataset.loc);
-      moveCount++;
-      //if (winCheck(xChoices) === true) {
-      //  alert(turn + ' is the winner!');
-      //  document.location.reload()
-      //}
-      moveCheck(moveCount,xChoices,turn);
-      turn = 'o'
+
+    play(target,turn);
+
+    if(turn === 'x'){
+      turn =  'o'
     } else {
-      target.childNodes[0].className = "fa fa-circle-o fa-5x";
-      target.childNodes[0].style.display = 'inline-block';
-      oChoices.push(target.dataset.loc);
-      moveCount++;
-      //if (winCheck(oChoices) === true) {
-      //  alert(turn + ' is the winner!');
-      //  document.location.reload()
-      //}
-      moveCheck(moveCount,xChoices,turn);
       turn = 'x'
     }
+
+    //if (turn === 'x') {
+    //  target.dataset.avail = 'no';
+    //  target.childNodes[0].className = "fa fa-times fa-5x";
+    //  target.childNodes[0].style.display = 'inline-block';
+    //  xChoices.push(target.dataset.loc);
+    //  moveCount++;
+    //  //if (winCheck(xChoices) === true) {
+    //  //  alert(turn + ' is the winner!');
+    //  //  document.location.reload()
+    //  //}
+    //  moveCheck(moveCount,xChoices,turn);
+    //  turn = 'o'
+    //} else {
+    //  target.childNodes[0].className = "fa fa-circle-o fa-5x";
+    //  target.childNodes[0].style.display = 'inline-block';
+    //  oChoices.push(target.dataset.loc);
+    //  moveCount++;
+    //  //if (winCheck(oChoices) === true) {
+    //  //  alert(turn + ' is the winner!');
+    //  //  document.location.reload()
+    //  //}
+    //  moveCheck(moveCount,xChoices,turn);
+    //  turn = 'x'
+    //}
 
 
   }
 });
-
-
-var arr =
-  ['x', 'x', 'x', 'o', 'o', 'x', 'o', 'o', 'x'];
-
-var wins = {};
 
 function winCheck(arr) {
   var combos = {
@@ -102,7 +118,6 @@ function winCheck(arr) {
     })
   }
   return result;
-
 }
 
 
